00bb69ef26ac90629f4f9684d4be3f4ebe53945a
zGiada <giada.zuccolo@studenti.unipd.it>
2023-08-30 13:50:46 +0200

    create style.js and change background img

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 import SimpleSvgComponent from './components/SimpleSvgComponent';
+import { styles } from "./Style";
 
 
 export default function App() { 
@@ -59,7 +60,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require('./img/bg.jpeg')}
+        source={require('./img/cloud.jpg')}
         style={styles.backgroundImage}
       >
         <View style={{
@@ -95,48 +96,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center', // Center the content inside the container
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  navBar: {
-    flexDirection: 'row', // Horizontal layout
-    justifyContent: 'space-around', // Evenly distribute buttons
-    alignItems: 'center', // Center the buttons vertically
-    backgroundColor: '#333', // Navbar background color
-    height: 150, // Adjust the height as needed
-    position: 'absolute',
-    bottom: 0, // Fixed to the bottom
-    left: 0,
-    right: 0,
-  },
-  navButton: {
-    backgroundColor: 'gray', // Set button background color
-    borderColor: '#444', // Border color of the button
-    borderWidth: 1, // Border width of the button
-    flex: 1, // Equal width for each button
-    alignItems: 'center', // Center the button's content
-    paddingVertical: 25, // Adjust the vertical padding
-  },
-  buttonText: {
-    color: 'white', // Text color of the button
-    fontSize: 14,
-    textAlign: 'center',
-  },
-});
