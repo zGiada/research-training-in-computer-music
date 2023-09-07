9de4fad1c6ceebdfef1aad186c460bf88f55227d
zGiada <giada.zuccolo@studenti.unipd.it>
2023-09-07 13:54:25 +0200

    create file to work into develop

---
+++ Style.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center', // Center the content inside the container
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    navBar: {
+        flexDirection: 'row', // Horizontal layout
+        justifyContent: 'space-around', // Evenly distribute buttons
+        alignItems: 'center', // Center the buttons vertically
+        backgroundColor: '#333', // Navbar background color
+        height: DEVICE_HEIGHT/5, // Adjust the height as needed
+        position: 'absolute',
+        bottom: 0, // Fixed to the bottom
+        left: 0,
+        right: 0,
+    },
+    navButton: {
+        backgroundColor: 'gray', // Set button background color
+        borderColor: '#444', // Border color of the button
+        borderWidth: 1, // Border width of the button
+        flex: 1, // Equal width for each button
+        alignItems: 'center', // Center the button's content
+        paddingVertical: 25, // Adjust the vertical padding
+    },
+    buttonText: {
+        color: 'white', // Text color of the button
+        fontSize: 14,
+        textAlign: 'center',
+    }
+
+});
+
+export { styles }
