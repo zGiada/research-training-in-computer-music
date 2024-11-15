360a45912498b31ff44c6800189272b4866a4f8a
zGiada <giada.zuccolo@studenti.unipd.it>
2024-11-15 20:36:34 +0100

    deploy on firebase for the new home gui

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,7 +6,7 @@ import SunAwake from "../components/sunAwake";
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center text-neutral-content overflow-y-auto">
-  {/* Header */}
+      {/* Header */}
       <Header />
 
       {/* Hero Section with Title and Content */}
@@ -19,137 +19,163 @@ export default function About() {
           microphone, the sun will change.
         </p>
         <p className="mb-10 mx-auto max-w-[80%] leading-none tracking-tight text-small-desc">
-          But, <b>how does it do?</b> Let's see it together!
+          But, <b>how does it do?</b> Let&apos;s see it together!
         </p>
 
         {/* Card Section */}
         <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 max-w-[80%]">
           <div className="card bg-gradient-card border-solid border-[2px] border-light-blue-500 text-sky-50">
             <div className="card-body">
-              <h2 className="card-title">The sun is <b>asleep</b></h2>
+              <h2 className="card-title">
+                The sun is <b>asleep</b>
+              </h2>
               <p>when the microphone does not recognize any voice sound.</p>
-              <SunSleep svgColor={"yellow"} rad={25} yCoordinate={-10} heightSpaceSun={"12vh"} />
+              <SunSleep
+                svgColor={"yellow"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
             </div>
           </div>
           <div className="card bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">The sun is <b>awake</b></h2>
+              <h2 className="card-title">
+                The sun is <b>awake</b>
+              </h2>
               <p>when the microphone captures the sound of a voice.</p>
-              <SunAwake svgColor={"yellow"} rad={25} yCoordinate={-10} heightSpaceSun={"12vh"} />
+              <SunAwake
+                svgColor={"yellow"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
             </div>
           </div>
         </div>
         <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 max-w-[80%]">
           <div className="card bg-gradient-card border-solid border-[2px] border-light-blue-500 text-sky-50 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">The sun is <b>gets smaller</b></h2>
+              <h2 className="card-title">
+                The sun is <b>gets smaller</b>
+              </h2>
               <p>as the sound becomes softer</p>
-              <SunAwake svgColor={"yellow"} rad={15} yCoordinate={-10} heightSpaceSun={"18vh"}/>
+              <SunAwake
+                svgColor={"yellow"}
+                rad={15}
+                yCoordinate={-10}
+                heightSpaceSun={"18vh"}
+              />
             </div>
           </div>
           <div className="card bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-          <div className="card-body">
-              <h2 className="card-title">The sun is <b>gets bigger</b></h2>
+            <div className="card-body">
+              <h2 className="card-title">
+                The sun is <b>gets bigger</b>
+              </h2>
               <p>as the sound becomes louder</p>
-              <SunAwake svgColor={"yellow"} rad={40} yCoordinate={-10} heightSpaceSun={"18vh"}/>
+              <SunAwake
+                svgColor={"yellow"}
+                rad={40}
+                yCoordinate={-10}
+                heightSpaceSun={"18vh"}
+              />
             </div>
           </div>
         </div>
 
         <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 max-w-[80%]">
-  <div>
-    <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">
-          Sun is <b>red</b>
-        </h2>
-        <p>
-          if the sound of the <b>vowel A</b> is recognized
-        </p>
-      </div>
-      <SunAwake
-        svgColor={"red"}
-        rad={25}
-        yCoordinate={-10}
-        heightSpaceSun={"12vh"}
-      />
-    </div>
-  </div>
-  <div>
-    <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">
-        Sun is <b>green</b>
-        </h2>
-        <p>
-          if the sound of the <b>vowel E</b> is recognized
-        </p>
-      </div>
-      <SunAwake
-        svgColor={"#4CC94C"}
-        rad={25}
-        yCoordinate={-10}
-        heightSpaceSun={"12vh"}
-      />
-    </div>
-  </div>
-  <div>
-    <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">
-        Sun is <b>blue</b>
-        </h2>
-        <p>
-          if the sound of the <b>vowel I</b> is recognized
-        </p>
-      </div>
-      <SunAwake
-        svgColor={"blue"}
-        rad={25}
-        yCoordinate={-10}
-        heightSpaceSun={"12vh"}
-      />
-    </div>
-  </div>
-  <div>
-    <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">
-        Sun is <b>orange</b>
-        </h2>
-        <p>
-          if the sound of the <b>vowel O</b> is recognized
-        </p>
-      </div>
-      <SunAwake
-        svgColor={"orange"}
-        rad={25}
-        yCoordinate={-10}
-        heightSpaceSun={"12vh"}
-      />
-    </div>
-  </div>
-  <div>
-    <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">
-        Sun is <b>gray</b>
-        </h2>
-        <p>
-          if the sound of the <b>vowel U</b> is recognized
-        </p>
-      </div>
-      <SunAwake
-        svgColor={"#C0C0C0"}
-        rad={25}
-        yCoordinate={-10}
-        heightSpaceSun={"12vh"}
-      />
-    </div>
-  </div>
-</div>
-
-
+          <div>
+            <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
+                  Sun is <b>red</b>
+                </h2>
+                <p>
+                  if the sound of the <b>vowel A</b> is recognized
+                </p>
+              </div>
+              <SunAwake
+                svgColor={"red"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
+                  Sun is <b>green</b>
+                </h2>
+                <p>
+                  if the sound of the <b>vowel E</b> is recognized
+                </p>
+              </div>
+              <SunAwake
+                svgColor={"#4CC94C"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
+                  Sun is <b>blue</b>
+                </h2>
+                <p>
+                  if the sound of the <b>vowel I</b> is recognized
+                </p>
+              </div>
+              <SunAwake
+                svgColor={"blue"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
+                  Sun is <b>orange</b>
+                </h2>
+                <p>
+                  if the sound of the <b>vowel O</b> is recognized
+                </p>
+              </div>
+              <SunAwake
+                svgColor={"orange"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="card w-auto bg-gradient-card border-solid border-[2px] border-light-blue-500 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">
+                  Sun is <b>gray</b>
+                </h2>
+                <p>
+                  if the sound of the <b>vowel U</b> is recognized
+                </p>
+              </div>
+              <SunAwake
+                svgColor={"#C0C0C0"}
+                rad={25}
+                yCoordinate={-10}
+                heightSpaceSun={"12vh"}
+              />
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Background Wave */}
--- app/page.tsx
+++ app/page.tsx
@@ -7,53 +7,58 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col text-neutral-content relative">
-  {/* Header */}
-  <Header />
- {/* Background Wave */}
- <div className="bg-wave absolute bottom-0 w-full h-[355px]"></div>
+      {/* Header */}
+      <Header />
+      {/* Background Wave */}
+      <div className="bg-wave absolute bottom-0 w-full h-[355px]"></div>
 
-  {/* Hero Section with Image, Text, and SunAwake Component */}
-  <div className="flex flex-col items-center justify-center text-center text-white h-full">
-    <Image
-      src="/soundRise-scritta.svg"
-      alt="SoundRise Logo"
-      height={dimsFunctions.height/4}
-      width={dimsFunctions.height/4}
-      className="mx-auto mt-2"
-    />
-    <h1 className="font-extrabold mb-5 big-title leading-none tracking-tight title-settings">
-      A New Sunrise for Speech Therapy:{" "}
-      <span className="divisore"></span>Development of{" "}
-      <span className="text-yellow-500"> SoundRise 2.0 </span> Application
-    </h1>
-    <p className="mb-2 leading-none tracking-tight text-small-desc">
-    SoundRise is an educational application with the aim of assisting people, especially children and adolescents, in their vocal learning journey. Designed to be used as an extra support to a speech therapy pathway for young patients with hearing and communication difficulties, it combines the worlds of technology and education, with a focus on inclusivity.
-              <br /><br />
-              A friendly sun represents the tonal and timbral characteristics of the user's voice. When a vocal is emitted, the sun wakes up and smiles, and, based on note pitch, intensity, and timbre, changes its vertical position, size, and color, all in real time. This visual system helps to understand how one's voice works.
-    </p>
-    <div className="relative flex max-h-screen flex-col items-center">
-    <SunAwake
-      svgColor={"yellow"}
-      rad={dimsFunctions.minRad * 1.5}
-      yCoordinate={-10}  // Rende il sole più visibile
-      heightSpaceSun={"20vh"}  // Riduce lo spazio di movimento
-    />
+      {/* Hero Section with Image, Text, and SunAwake Component */}
+      <div className="flex flex-col items-center justify-center text-center text-white h-full">
+        <Image
+          src="/soundRise-scritta.svg"
+          alt="SoundRise Logo"
+          height={dimsFunctions.height / 4}
+          width={dimsFunctions.height / 4}
+          className="mx-auto mt-2"
+        />
+        <h1 className="font-extrabold mb-5 big-title leading-none tracking-tight title-settings">
+          A New Sunrise for Speech Therapy: <span className="divisore"></span>
+          Development of{" "}
+          <span className="text-yellow-500"> SoundRise 2.0 </span> Application
+        </h1>
+        <p className="mb-2 leading-none tracking-tight text-small-desc">
+          SoundRise is an educational application with the aim of assisting
+          people, especially children and adolescents, in their vocal learning
+          journey. Designed to be used as an extra support to a speech therapy
+          pathway for young patients with hearing and communication
+          difficulties, it combines the worlds of technology and education, with
+          a focus on inclusivity.
+          <br />
+          <br />A friendly sun represents the tonal and timbral characteristics
+          of the user&apos;s voice. When a vocal is emitted, the sun wakes up
+          and smiles, and, based on note pitch, intensity, and timbre, changes
+          its vertical position, size, and color, all in real time. This visual
+          system helps to understand how one&apos;s voice works.
+        </p>
+        <div className="relative flex max-h-screen flex-col items-center">
+          <SunAwake
+            svgColor={"yellow"}
+            rad={dimsFunctions.minRad * 1.5}
+            yCoordinate={-10} // Rende il sole più visibile
+            heightSpaceSun={"20vh"} // Riduce lo spazio di movimento
+          />
+        </div>
+      </div>
 
-    </div>
-  </div>
-
- 
-  {/* Footer */}
-  <footer className="w-full flex justify-center text-white p-3 bg-black z-10">
-    <a
-      href="https://www.gnu.org/licenses/gpl-3.0.txt"
-      className="text-sm font-mono underline text-gray-300 hover:text-gray-400"
-    >
-      GNU General Public License v3.0
-    </a>
-  </footer>
-
-</main>
-
+      {/* Footer */}
+      <footer className="w-full flex justify-center text-white p-3 bg-black z-10">
+        <a
+          href="https://www.gnu.org/licenses/gpl-3.0.txt"
+          className="text-sm font-mono underline text-gray-300 hover:text-gray-400"
+        >
+          GNU General Public License v3.0
+        </a>
+      </footer>
+    </main>
   );
 }
